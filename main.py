a3b75f3db323018f2e3bc54fc56cccd1abe8a540
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2022-09-24 13:32:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,8 @@ elif add_selectbox == 'Output parameters prediction and Anomaly Prediction':
       if submit:
           principalComponents=pcaLoaded.transform(pca_df)
           pca_df_display=pd.DataFrame(principalComponents,columns=['principal component 1','principal component 1','principal component 1']
-          st.write('Principal components',pca_df_display)
+          st.write('Principal components')
+          pca_df_display
 elif add_selectbox == 'Team':
     
     st.subheader('Teamates')
